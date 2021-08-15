8b5ea23406475e5def25c1a8707176d4274b9cbe
Eren SARAÇ <mailerensarac+github@gmail.com>
2021-08-15 23:48:10 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+function myFunction2() {
+    document.getElementById("light-img").svg = "logo.svg";
+}
