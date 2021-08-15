1b620347303b6cbdb6e785b8ff2f1e31b8734d17
Eren SARAÇ <mailerensarac+github@gmail.com>
2021-08-16 00:18:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,46 +5,4 @@ function lightMode() {
 function darkMode() {
     var element = document.body;
     element.classList.toggle("darkMode");
-}function lightMode() {
-    var element = document.body;
-    element.classList.toggle("lightMode");
-}
-function darkMode() {
-    var element = document.body;
-    element.classList.toggle("darkMode");
-}function lightMode() {
-    var element = document.body;
-    element.classList.toggle("lightMode");
-}
-function darkMode() {
-    var element = document.body;
-    element.classList.toggle("darkMode");
-}function lightMode() {
-    var element = document.body;
-    element.classList.toggle("lightMode");
-}
-function darkMode() {
-    var element = document.body;
-    element.classList.toggle("darkMode");
-}function lightMode() {
-    var element = document.body;
-    element.classList.toggle("lightMode");
-}
-function darkMode() {
-    var element = document.body;
-    element.classList.toggle("darkMode");
-}function lightMode() {
-    var element = document.body;
-    element.classList.toggle("lightMode");
-}
-function darkMode() {
-    var element = document.body;
-    element.classList.toggle("darkMode");
-}function lightMode() {
-    var element = document.body;
-    element.classList.toggle("lightMode");
-}
-function darkMode() {
-    var element = document.body;
-    element.classList.toggle("darkMode");
 }
