8e84832b6f0e38c233f1740be9525aa2106489ab
Eren SARAÇ <mailerensarac+github@gmail.com>
2021-08-18 01:38:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function lightMode1() {
+    var element = document.body;
+    element.classList.toggle("lightMode");
+}
+function darkMod2e() {
+    var element = document.body;
+    element.classList.toggle("darkMode");
+}
 
