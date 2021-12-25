8a7a71fe5f75e5d990241c9b2076482a51dc21a2
Eren Saraç <esarac@outlook.com.tr>
2021-12-26 02:00:23 +0300

    Create tabmenu.js

---
+++ script/tabmenu.js
@@ -0,0 +1,12 @@
+var tabMenu = document.getElementById("tabMenu"),
+    tabMenuBg = document.getElementById("tabMenuBg");
+
+function openTabMenu() {
+    tabMenu.style.display = "flex";
+    tabMenuBg.style.display = "block";
+}
+
+function closeTabMenu() {
+    tabMenu.style.display = "none";
+    tabMenuBg.style.display = "none";
+}
