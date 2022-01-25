979109f77fa00d45a99c845ac5cb8b0a59f082cf
Eren Saraç <esarac@outlook.com.tr>
2022-01-25 05:18:37 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-let pageBody = document.getElementById("main-body") let themeIcon = document.getElementById("theme-icon") function theme(){ pageBody.classList.toggle("light") themeIcon.classList.toggle("fa-sun")}
+let pageBody = document.getElementById("main-body")
+let themeIcon = document.getElementById("theme-icon")
+
+function theme(){
+    pageBody.classList.toggle("light")
+    themeIcon.classList.toggle("fa-sun")
+}
