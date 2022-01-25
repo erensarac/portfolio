f85022c6997f9e6f940a220ccc9e68dab67ef04b
Eren Saraç <esarac@outlook.com.tr>
2022-01-25 05:16:13 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,1 @@
+let pageBody = document.getElementById("main-body") let themeIcon = document.getElementById("theme-icon") function theme(){ pageBody.classList.toggle("light") themeIcon.classList.toggle("fa-sun")}
