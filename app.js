5d09064d682ec5fe0f6f676b665ed61872c2ce59
Eren Saraç <esarac@outlook.com.tr>
2022-02-09 22:10:45 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,1 @@
-let eren = document.getElementById("main-body")
-let themeIcon = document.getElementById("theme-icon")
-
-function theme(){
-    eren.classList.toggle("light")
-    themeIcon.classList.toggle("fa-sun")
-}
+console.log("Hello World!")
