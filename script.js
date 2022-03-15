0a890b43c16efe2b384a9d251697f9c08067baa6
Eren Saraç <esarac@outlook.com.tr>
2022-03-15 21:58:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ const pageBody = document.querySelector("body")
 const themeIcon = document.querySelector("#theme-icon")
 
 themeIcon.addEventListener("click", () => {
-  if (themeIcon.classList.contains("ri-sun-line")){
+  if (themeIcon.classList.contains("ri-moon-line")){
+    themeIcon.classList.remove("ri-moon-line")
+    themeIcon.classList.add("ri-sun-line")
+    
+    pageBody.classList.remove("light-theme")
+    pageBody.classList.add("dark-theme")
+  } else {
     themeIcon.classList.remove("ri-sun-line")
     themeIcon.classList.add("ri-moon-line")
-    
-    pageBody.classList.add("light-theme")
-    pageBody.classList.remove("dark-theme")
-  } else {
-    themeIcon.classList.add("ri-sun-line")
-    themeIcon.classList.remove("ri-moon-line")
     
     pageBody.classList.remove("light-theme")
     pageBody.classList.add("dark-theme")
