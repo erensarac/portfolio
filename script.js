0793672e6e5b3adde02382ae78f265f5ab945838
Eren Saraç <esarac@outlook.com.tr>
2022-03-16 23:42:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
-  const pageBody = document.querySelector("body")
-  const themeIcon = document.querySelector("#theme-icon")
+const pageBody = document.querySelector("body")
+const themeIcon = document.querySelector("#theme-icon")
 
-  themeIcon.addEventListener("click", () => {
-    if (themeIcon.classList.contains("ri-moon-line")){
-      themeIcon.classList.add("ri-sun-line")
-      themeIcon.classList.remove("ri-moon-line")
+document.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("theme") === "dark")
+  {
+    makeDarkTheme()
+  } else {
+    makeLightTheme()
+  }
+})
 
-      pageBody.classList.add("dark-theme")
-      pageBody.classList.remove("light-theme")
-    } else {
-      themeIcon.classList.add("ri-moon-line")
-      themeIcon.classList.remove("ri-sun-line")
+themeIcon.addEventListener("click", () => {
+  if (themeIcon.classList.contains("ri-moon-line"))
+  {
+    makeDarkTheme()
+  } else {
+    makeLightTheme()
+  }
+})
 
-      pageBody.classList.add("light-theme")
-      pageBody.classList.remove("dark-theme")
+function makeDarkTheme(){
+  console.log("sa");
+  themeIcon.classList.add("ri-sun-line")
+  themeIcon.classList.remove("ri-moon-line")
+  pageBody.classList.add("dark-theme")
+  pageBody.classList.remove("light-theme")
+  localStorage.setItem("theme", "dark")
+}
 
-    }  
-  })
+function makeLightTheme(){
+  console.log("as");
+  themeIcon.classList.add("ri-moon-line")
+  themeIcon.classList.remove("ri-sun-line")
+  pageBody.classList.add("light-theme")
+  pageBody.classList.remove("dark-theme")
+  localStorage.setItem("theme", "light")
+}
