a4992a7bd8f8e0bf047a015b7345d868ef40b468
Eren Saraç <esarac@outlook.com.tr>
2022-03-16 23:25:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-const pageBody = document.querySelector("body")
-const themeIcon = document.querySelector("#theme-icon")
+  const pageBody = document.querySelector("body")
+  const themeIcon = document.querySelector("#theme-icon")
 
-themeIcon.addEventListener("click", () => {
-  if (themeIcon.classList.contains("ri-moon-line")){
-    themeIcon.classList.remove("ri-moon-line")
-    themeIcon.classList.add("ri-sun-line")
-    
-    pageBody.classList.remove("light-theme")
-    pageBody.classList.add("dark-theme")
-  } else {
-    themeIcon.classList.remove("ri-sun-line")
-    themeIcon.classList.add("ri-moon-line")
-    
-    pageBody.classList.remove("light-theme")
-    pageBody.classList.add("dark-theme")
-    
-  }  
-})
+  themeIcon.addEventListener("click", () => {
+    if (themeIcon.classList.contains("ri-moon-line")){
+      themeIcon.classList.add("ri-sun-line")
+      themeIcon.classList.remove("ri-moon-line")
+
+      pageBody.classList.add("dark-theme")
+      pageBody.classList.remove("light-theme")
+    } else {
+      themeIcon.classList.add("ri-moon-line")
+      themeIcon.classList.remove("ri-sun-line")
+
+      pageBody.classList.add("light-theme")
+      pageBody.classList.remove("dark-theme")
+
+    }  
+  })
